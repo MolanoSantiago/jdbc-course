f1c256fa8a1b981d421d4589932fd5327d387821
MolanoSantiago <s.molano0818@gmail.com>
2023-11-01 22:17:05 -0500

    Add my first query

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.sql.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Connection myConnection = null;
+        Statement myStatement = null;
+        ResultSet myResultSet = null;
+
+        try {
+            myConnection = DriverManager.getConnection("jdbc:mysql://localhost:3306/crm-react", "root", "");
+            System.out.println("We are connected successfully");
+
+            myStatement = myConnection.createStatement();
+            myResultSet = myStatement.executeQuery("SELECT * FROM users");
+
+            while (myResultSet.next()) {
+                System.out.println(myResultSet.getString("name"));
+            }
+        } catch (SQLException e) {
+            System.out.println("Something bad has happened");
+            throw new RuntimeException(e);
+        }
     }
 }
